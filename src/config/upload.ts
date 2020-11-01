16c16c2d1ec6c7309aa2799ae7c66cc2677c8aae
Felipe Eduardo <felipe.e.w@hotmail.com>
2020-10-31 23:22:57 -0300

    api 1.0 ok

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -5,7 +5,7 @@ export default {
   storage: multer.diskStorage({
     destination: path.join(__dirname, '..', '..', 'uploads'),
     filename: (request, file, cb) => {
-      const filename = `${Date.now()}-${file.originalname}`;
+      const filename = `${Date.now()}-${file.originalname.split('').filter(e => e.trim().length).join('')}`;
 
       cb(null, filename);
     },
